038f61c362f1396835c6c559795c528ff1433e7e
sarpong <abasimisarpong@rocketmail.com>
2019-05-01 16:27:46 +0100

    Weather object implemented

--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,17 @@
 function Weather(){};
 
 Weather.prototype = {
-  isStormy: function(){
+  stormyWeather: function(){
     return(Math.floor((Math.random() * 10) + 1));
+  },
+
+  isStormy: function(){
+    if(this.stormyWeather() > 6){
+      return('Bad Weather can not land.');
+    }else{
+      return('You can land anytime because the weather is great.'); 
+    }
   }
 }
+
+module.exports = Weather;
